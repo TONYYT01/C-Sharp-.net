82cd888c09b8f1d0e7b1c0508a4351d76a48c519
Naveen <kolanaveen797@gmail.com>
2026-09-01 20:59:35 +0530

    Add project files.

---
+++ Learn/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    class Program
+    {
+        static void Main(string[] args  )
+        {
+            Random random = new Random();
+            int a = random.Next(0, 9);
+            int b = random.Next(0, 9);
+            int c = random.Next(0, 9);
+            Console.WriteLine(a+" "+b+" "+c);
+            Console.ReadKey();
+        }
+    }
+}
