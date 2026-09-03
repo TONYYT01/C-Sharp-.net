a82bf2d1bbc87ad4579b1539f29ceeb043bd6e64
Naveen <kolanaveen797@gmail.com>
2026-09-03 21:55:13 +0530

    hiii

--- Learn/Program.cs
+++ Learn/Program.cs
@@ -10,11 +10,52 @@ namespace Learn
     {
         static void Main(string[] args  )
         {
+            Console.WriteLine("Hello Buddy !");
             Random random = new Random();
-            int a = random.Next(0, 9);
-            int b = random.Next(0, 9);
-            int c = random.Next(0, 9);
-            Console.WriteLine(a+" "+b+" "+c);
+            int number;
+            int guess;
+            int guesses;
+            Boolean play = true;
+            string response = "";
+            int min = 1;
+            int max = 100;
+
+            while (play)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Enter the number : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess" + guess);
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + "is to high");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + "is to Low");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Number : " + number);
+                Console.WriteLine("You Win!");
+                Console.WriteLine("IF you want to play again : (Y/N)");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+                if (response == "Y")
+                {
+                    play = true;
+                }
+                else if(response == "N"){
+                    play = false;
+                }
+            }
+            Console.WriteLine("Thanks for playing : ");
             Console.ReadKey();
         }
     }
